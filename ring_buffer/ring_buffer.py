7cef9ae6ee72102c7400d9ebf3f5d457cc41629e
Serina Grill <gril0044@umn.edu>
2020-07-31 21:38:17 -0400

    part 1 complete, 35 mins in

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,50 @@
 class RingBuffer:
+
+    counter = 0
+
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.index = 0
 
     def append(self, item):
-        pass
+
+        if len(self.storage) == self.capacity:
+            self.storage[self.index] = item
+
+        else:
+            self.storage.append(item)
+
+        # add item to the index after the one it's at
+        self.index = (self.index + 1) % self.capacity
+    
 
     def get(self):
-        pass
+        ''' Returns all elements in the buffer 
+        as a list in their given order
+        '''
+        return self.storage
+
+
+# buffer = RingBuffer(3)
+
+# buffer.get()
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# buffer.get()  
+
+# buffer.append('d')
+
+# buffer.get()   
+
+# buffer.append('e')
+# buffer.append('f')
+
+# buffer.get() 
+
+buffer = RingBuffer(5)
+buffer.append('a')
+print(buffer.get())
