e5df784dfff28f853f0202cb92e51b6cc03575d0
Serina Grill <gril0044@umn.edu>
2020-08-01 12:31:14 -0400

    SC MVP

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,122 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # the head is 5, in the input is head,
+        # the prev is None
+        # none 5 4 3 2 1
+        
+
+        # if there is a next node 
+        while node:
+
+            # make the next node the new node 
+            #1
+            next_node = node.next_node
+
+            # set the node's (5) next to  
+            # None is the current's next node
+            #3 is the next node.next_node
+            node.next_node = prev
+            # 2 is the prev
+            prev = node
+            # 1 is the node
+            node = next_node
+        self.head = prev
+            
+
+
+        # if node.next_node:
+        #     node.next_node = prev
+        #     return self.reverse_list(node.next_node, node)
+
+        # else:
+            # self.head = current
+        
+        # if node.next_node is None:
+        #     self.head = node
+        #     return self.head
+        # else: 
+        #     current = node
+        #     node.next_node = prev
+            
+        #     if current.next_node:
+        #         self.reverse_list(current.next_node, node)
+
+        #     else:
+        #         self.head = current
+
+
+
+
+        # node = self.head
+
+        # if node.next_node is not None:
+        #     temp = node.next_node
+        #     node.next = prev
+        #     self.reverse_list(temp, node)
+        # else:
+        #     return node 
+
+
+
+
+        # cur_node = self.head
+        # next_node = cur_node.next_node
+
+        # cur_node.set_next(None)
+        # self.head = cur_node
+        # while next_node is not None:
+        #     prev_node = cur_node
+        #     cur_node = next_node
+        #     next_node = cur_node.get_next()
+        #     cur_node.set_next(prev_node)
+
+
+        
+        
+
+
+       
+
+
+
+
+
+
+
+
+        # while node.next_node is not None:
+
+    
+        # if node.next_node:
+        #     self.reverse_list(node.next_node, node)
+
+
+        # if node.next_node:
+        #     temp = node.next_node
+        #     node.next_node = prev
+        #     return reverse_list(temp, node)
+        # else:
+        #     return node.get_value()
+
+        #     # the 2 is now after the 1
+        #     node.next_node = prev
+        #     self.head = node
+
+
+        # new_root = self.reverse_list(node.next_node, node)
+        # node.next_node = prev
+        # return new_root
+
+        
+        # # node.prev = 1
+
+        
+
+
+        
+
+
+        
+
+        
